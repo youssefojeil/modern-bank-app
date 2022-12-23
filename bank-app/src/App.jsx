a9031ca8446bd1c9fd41d6137a203f29a8a7f06e
unknown <youssef.ojeil@hotmail.com>
2022-12-23 18:56:45 -0500

    complete getStarted component

--- bank-app/src/App.jsx
+++ bank-app/src/App.jsx
@@ -23,7 +23,9 @@ const App = () => {
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
